ba27f88cef2483650fc30c6a7315ef4b440275bd
Vasily Gotovko <vasily.gotovko@gmail.com>
2013-02-20 13:42:02 +0300

    Add role permissions

--- init.rb
+++ init.rb
@@ -7,4 +7,13 @@ Redmine::Plugin.register :redmine_custom_overview do
   version     '0.0.1'
   url         'https://github.com/twinslash/redmine_custom_overview'
   author_url  'http://twinslash.com'
+
+  project_module :overview do
+    permission :wiki, { :wiki => nil }
+    permission :wall, { :wall => nil }
+    permission :activity, { :activity => nil }
+    permission :roadmap, { :roadmap => nil }
+    permission :members, { :members => nil }
+    permission :latest_news, { :latest_news => nil }
+  end
 end

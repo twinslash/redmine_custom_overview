958f4497194a703be46775188e616fdd63dc881d
Vasily Gotovko <vasily.gotovko@gmail.com>
2013-02-20 12:18:13 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# redmine_custom_overview
+
+Description goes here
---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine_custom_overview'
+
+Redmine::Plugin.register :redmine_custom_overview do
+  name 'Redmine Custom Overview'
+  author      '//Twinslash'
+  description "Plugin change project's overview"
+  version     '0.0.1'
+  url         'https://github.com/twinslash/redmine_custom_overview'
+  author_url  'http://twinslash.com'
+end
